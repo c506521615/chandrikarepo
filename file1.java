6de5b0f867f76342a11348b502ee0966180e3f27
chandrika sahoo <chandrikasahoo@gmail.com>
2022-01-29 16:46:00 +0000

    my first commit of the source code

---
+++ file1.java
@@ -0,0 +1,9 @@
+total 3248
+-rw-rw-r-- 1 chandrikasahoog chandrikasahoog       7 Jan 29 16:29 check_idle
+drwxr-xr-x 2 jenkins         jenkins            4096 Jan 29 15:34 hsperfdata_jenkins
+drwxr-xr-x 2 root            root               4096 Jan 29 15:35 hsperfdata_root
+drwx------ 2 jenkins         jenkins            4096 Jan 29 15:34 jetty-0_0_0_0-8080-war-_-any-1292314353389938880
+drwx------ 2 chandrikasahoog chandrikasahoog    4096 Jan 29 15:37 ssh-92VTPATNkj0V
+drwx------ 3 root            root               4096 Jan 29 15:34 systemd-private-e644738d80184844b24792b8f348b064-rtkit-daemon.service-t8Hf2K
+drwx------ 3 root            root               4096 Jan 29 15:34 systemd-private-e644738d80184844b24792b8f348b064-systemd-timesyncd.service-hXnBeO
+-rw-r--r-- 1 jenkins         jenkins         3293397 Jan 29 15:34 winstone372141903992687402.jar
